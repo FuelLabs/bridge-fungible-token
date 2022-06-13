b9cc8283522bfdec2700e993dfe47bceb84f7748
simonr0204 <simon.roberts@fuel.sh>
2022-06-13 15:49:33 -0400

    test evaluation with trivial predicate

--- POC/script/tests/harness.rs
+++ POC/script/tests/harness.rs
@@ -9,39 +9,54 @@ use fuel_crypto::Hasher;
 use fuels_signers::provider::Provider;
 
 
+
+// Predicate testing:
+
+// 0.1 Write a script that sends coin to an address, and compile. Get script hash
+// 0.2 Write a predicate that expects this script hash. Get predicate hash
+// 1. send some coins to the predicate hash 
+// 2. Build corresponding script transaction that spends Coin input, provide predicate along with input (what about witness?)
+
+
+
+
 #[tokio::test]
 async fn run_script() {
 
-    let server = FuelService::new_node(Config::local_node()).await.unwrap();
-    let client = FuelClient::from(server.bound_address);
+    // Set up a wallet and send some native asset to the predicate hash
+    let native_asset: AssetId = Default::default();
+    let mut config = Config::local_node();
+    config.predicates = true;
+    let wallet = launch_custom_provider_and_get_single_wallet(config).await;
+    let wallet_balance = wallet.get_asset_balance(&native_asset).await.unwrap();
+    println!("Wallet balance: {}", wallet_balance);
 
+    // Get provider and client
+    let provider = wallet.get_provider().unwrap();
+    let client = &provider.client;
+
+    // Get script binary and hash
     let script_binary = std::fs::read("../script/out/debug/script.bin").unwrap();
     let script_hash = Hasher::hash(&script_binary);
     println!("Script hash   : 0x{}", script_hash);
 
+    // Get predicate binary and hash
     let predicate_binary = std::fs::read("../predicate/out/debug/predicate.bin").unwrap();
     let predicate_hash = Hasher::hash(predicate_binary.clone());
     let predicate_hash_as_address = fuels::tx::Address::from(*predicate_hash);
     println!("Predicate hash: 0x{}", predicate_hash);
 
-    // Set up a wallet and send some native asset to the predicate hash
-    let native_asset: AssetId = Default::default();
-    let wallet = launch_provider_and_get_single_wallet().await;
-    let wallet_balance = wallet.get_asset_balance(&native_asset).await.unwrap();
-    println!("Wallet balance: {}", wallet_balance);
+    // Transfer some coins to the predicate hash
     let receipt = wallet.transfer(&predicate_hash_as_address, 1000, native_asset, TxParameters::default()).await.unwrap();
 
-    // Inspect predicate balance
-    let provider = wallet.get_provider().unwrap();
+    // Inspect predicate hash balance
     let predicate_balance = provider.get_asset_balance(&predicate_hash_as_address, native_asset).await.unwrap();
     println!("Predicate balance: {}", predicate_balance);
 
     // Get predicate coin to spend
     let predicate_coin: UtxoId = provider.get_coins(&predicate_hash_as_address).await.unwrap()[0].utxo_id.clone().into();
-    println!("{:?}", predicate_coin);
 
-    // Need to configure inputs and outputs to send coins from predicate to another wallet. 
-    
+    // Configure inputs and outputs to send coins from predicate to another wallet. 
     // Coin input will have a predicate attached
     
     let i1 = Input::CoinPredicate {
@@ -55,12 +70,11 @@ async fn run_script() {
     };
     
 
-    // a variable output for the coin transfer
-    // a change output ?
+    // A variable output for the coin transfer
     let o1 = Output::Variable{
-        to: predicate_hash_as_address,
+        to: Address::zeroed(),
         amount: 0, 
-        asset_id: native_asset
+        asset_id: AssetId::default()
     };
 
 
@@ -73,7 +87,7 @@ async fn run_script() {
         script: script_binary, // Here we pass the compiled script into the transaction
         script_data: vec![],
         inputs: vec![i1],
-        outputs: vec![o1],
+        outputs: vec![o1, o1],
         witnesses: vec![vec![].into()],
         metadata: None,
     };
@@ -82,12 +96,4 @@ async fn run_script() {
 
     let receipts = script.call(&client).await.unwrap();
 }
-
-
-// Predicate testing:
-
-// 0.1 Write a script that sends coin to an address, and compile. Get script hash
-// 0.2 Write a predicate that expects this script hash. Get predicate hash
-// 1. send some coins to the predicate hash 
-// 2. Build corresponding script transaction that spends Coin input, provide predicate along with input (what about witness?)
 
